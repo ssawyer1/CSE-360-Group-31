f48eea7b979e10c8535178f0107bc4eee6500453
Ben Wallace <bwallace1123@gmail.com>
2021-11-04 16:09:05 -0700

    Update Doctor.java
    Added methods: addPatient, addNurse, addMessage, addNote, prescribeMedication, getMessage

--- src/application/Doctor.java
+++ src/application/Doctor.java
@@ -6,13 +6,16 @@ public class Doctor {
 	private String email;
 	private ArrayList<Patient> patientArrayList;
 	private ArrayList<Nurse> nurseArrayList;
+	private ArrayList<Message> messageArrayList;
 	
+	//Default constructor
 	Doctor()
 	{
 		this.phoneNumber = 0;
 		this.email = "";
 		this.patientArrayList = new ArrayList<Patient>();
 		this.nurseArrayList = new ArrayList<Nurse>();
+		this.messageArrayList = new ArrayList<Message>();
 	}
 	
 	Doctor(String email, int phoneNumber)
@@ -20,29 +23,89 @@ public class Doctor {
 		this.phoneNumber = phoneNumber;
 		this.email = email;
 		this.patientArrayList = new ArrayList<Patient>();
+		this.nurseArrayList = new ArrayList<Nurse>();
+		this.messageArrayList = new ArrayList<Message>();
 	}
 	
-	//viewPatientInformation searches the doctor's patientArrayList for patient and returns their information. 
+	//Methods:
+	
+	public void addPatient(Patient patient)
+	{
+		this.patientArrayList.add(patient);
+	}
+	
+	public void addNurse(Nurse nurse)
+	{
+		this.nurseArrayList.add(nurse);
+	}
+	
+	public void addMessage(Message message)
+	{
+		this.messageArrayList.add(message);
+	}
+	
+	public void addNote(String note, Patient patient) 
+	{
+		for (int i = 0; i < patientArrayList.size(); i++)
+		{
+			if(patientArrayList.get(i) == patient) 
+			{
+				//patientArrayList.get(i).addNote(note)
+			}
+		}
+	}
+	
+	public void getMessage() 
+	{
+		for (int i = 0; i < messageArrayList.size(); i++)
+		{
+			messageArrayList.get(i).getMessage();
+		}
+	}
+	
+	//viewPatientInformation iterates through the doctor's patientArrayList for patient and returns their information. 
 	public void viewPatientInformation(Patient patient)
 	{
 		for (int i = 0; i < patientArrayList.size(); i++)
 		{
 			if(patientArrayList.get(i) == patient) 
 			{
-				//patient->getInformation
+				//patientArrayList.get(i).getInformation()
 			}
 		}
 	}
 	
-	//viewMedicalRecord searches the doctor's patientArrayList for patient and returns their medical record.
+	//viewAllPatientInformation iterates through the doctor's patientArrayList and prints the patients information. 
+	public void viewAllPatientInformation()
+		{
+			for (int i = 0; i < patientArrayList.size(); i++)
+			{
+				//patientArrayList.get(i).getInformation()
+			}
+		}
+	
+	//viewMedicalRecord searches the doctor's patientArrayList for patient object and returns their medical record.
 	public void viewMedicalRecord(Patient patient)
 	{
 		for (int i = 0; i < patientArrayList.size(); i++)
 		{
 			if(patientArrayList.get(i) == patient) 
 			{
-				//patient->getMedicalRecord
+				//patient.get(i).getMedicalRecord()
 			}
 		}
 	}
+	
+	public void prescribeMedication(Patient patient, String medication)
+	{
+		for (int i = 0; i < patientArrayList.size(); i++)
+		{
+			if(patientArrayList.get(i) == patient)
+			{
+				//patientArrayList.get(i).addMedication(medication);
+			}
+		}
+	}
+	
 }
+
