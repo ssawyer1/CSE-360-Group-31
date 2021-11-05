35c9a85a20634c7ee5c0ff4a4c9db6a9591fb0b6
swerth12 <90576543+swerth12@users.noreply.github.com>
2021-11-04 18:22:09 -0700

    Update Nurse.java

--- src/application/Nurse.java
+++ src/application/Nurse.java
@@ -1,5 +1,36 @@
 package application;
 
 public class Nurse {
+  
+  private int phoneNumber;
+	private String email;
+  private String name;
+	private ArrayList<Patient> patientArrayList;
+	private ArrayList<Doctor> doctorArrayList;
+	private ArrayList<Message> messageArrayList;
+  
+  Nurse()
+  {
+    this.phoneNumber = 0;
+		this.email = "";
+    this.name = "";
+		this.patientArrayList = new ArrayList<Patient>();
+		this.doctorArrayList = new ArrayList<Doctor>();
+		this.messageArrayList = new ArrayList<Message>();
+	}
+  	
+  Nurse(String email, int phoneNumber, String name)
+	{
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+    this.name = name;
+		this.patientArrayList = new ArrayList<Patient>();
+		this.doctorArrayList = new ArrayList<Doctor>();
+		this.messageArrayList = new ArrayList<Message>();
+	}
+	
+  //Methods:
 
+ 
+  
 }
