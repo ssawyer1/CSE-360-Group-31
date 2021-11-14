bb2fe148796760b608c25d8f9fd421a98b533412
swerth12 <swerthe1@asu.edu>
2021-11-13 19:06:18 -0700

    c

--- src/application/Nurse.java
+++ src/application/Nurse.java
@@ -1,38 +1,61 @@
 package application;
-
 import java.util.ArrayList;
 
 public class Nurse {
-  
-  private int phoneNumber;
+	private int phoneNumber;
 	private String email;
-  private String name;
+	private String firstName;
+	private String lastName;
 	private ArrayList<Patient> patientArrayList;
 	private ArrayList<Doctor> doctorArrayList;
 	private ArrayList<Message> messageArrayList;
-  
-  Nurse()
-  {
-    this.phoneNumber = 0;
-		this.email = "";
-    this.name = "";
-		this.patientArrayList = new ArrayList<Patient>();
-		this.doctorArrayList = new ArrayList<Doctor>();
-		this.messageArrayList = new ArrayList<Message>();
-	}
-  	
-  Nurse(String email, int phoneNumber, String name)
-	{
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-    this.name = name;
-		this.patientArrayList = new ArrayList<Patient>();
-		this.doctorArrayList = new ArrayList<Doctor>();
-		this.messageArrayList = new ArrayList<Message>();
-	}
-	
-  //Methods:
 
- 
-  
+	Nurse()
+
+	 {
+		 this.phoneNumber = 0;
+		 this.email = "";
+		 this.firstName = "";
+		 this.lastName = "";
+		 this.patientArrayList = new ArrayList<Patient>();
+		 this.doctorArrayList = new ArrayList<Doctor>();
+		 this.messageArrayList = new ArrayList<Message>();
+	 }
+
+	Nurse(String email, int phoneNumber, String firstName, String lastName)
+	{ this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.patientArrayList = new ArrayList<Patient>();
+		this.doctorArrayList = new ArrayList<Doctor>();
+		this.messageArrayList = new ArrayList<Message>();
+	}
+
+	//methods
+	public void addPatient(Patient patient)
+	{
+		this.patientArrayList.add(patient);
+	}
+
+	public void addDoctor(Doctor doctor)
+	{
+		this.doctorArrayList.add(doctor);
+	}
+
+	public void addMessage(Message message)
+	{
+		this.messageArrayList.add(message);
+	}
+
+
+	public void getMessage()
+	{
+		for (int i = 0; i < messageArrayList.size(); i++)
+		{
+			messageArrayList.get(i).getMessage();
+		}
+	}
+
+
 }
